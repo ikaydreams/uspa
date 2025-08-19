95e50536e2f554639d4d51d3029b346641ed5fc8
Isaac Abakah <159209378+ikaydreams@users.noreply.github.com>
2025-08-19 08:27:26 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,11 +37,11 @@ const Header: React.FC = () => {
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0 flex items-center">
             <img
-              src="/U-Spa Flavicon.jpg"
+              src="/U-SPA.jpg"
               alt="U-Spa Logo"
               className="h-10 w-10 mr-3"
             />
-            <h1 className="text-2xl font-poppins font-bold text-foreground">U-Spa</h1>
+           
           </div>
 
           <div className="hidden md:block">
